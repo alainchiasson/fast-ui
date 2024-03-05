cace69f4a25f24454502951755214ddbf710a6d7
Alain Chiasson <alain@chiasson.org>
2024-03-05 10:05:49 -0500

    Bind to external IP of image.

--- app.py
+++ app.py
@@ -78,4 +78,4 @@ async def html_landing() -> HTMLResponse:
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", port=5000, log_level="info")
+    uvicorn.run("app:app", host="0.0.0.0", port=8080, log_level="info")
